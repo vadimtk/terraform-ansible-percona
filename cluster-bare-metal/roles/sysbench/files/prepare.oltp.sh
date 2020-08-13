00bdcf48c66c77c24b38dad3719c46aab4a26a5c
Vadim Tkachenko <vadim@cto-brain-sm-x8dtt-ccb963.tp.int.percona.com>
2020-08-13 16:11:12 -0400

    WIP

--- cluster-bare-metal/roles/sysbench/files/prepare.oltp.sh
+++ cluster-bare-metal/roles/sysbench/files/prepare.oltp.sh
@@ -1,2 +1,3 @@
 # prepares dataset, 90GB size
-sysbench oltp_read_only --tables=40 --table_size=10000000 --threads=40 --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --max-requests=0 --report-interval=1 --rand-type=uniform --mysql-db=sbtest --mysql-ssl=off --create_table_options='DEFAULT CHARSET=utf8mb4' prepare
+type="pareto"
+sysbench oltp_read_only --tables=40 --table_size=10000000 --threads=40 --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --max-requests=0 --report-interval=1  --mysql-db=sbtest --mysql-ssl=off --create_table_options='DEFAULT CHARSET=utf8mb4' --rand-type=$type prepare
--- cluster-bare-metal/roles/sysbench/files/run.oltp.sh
+++ cluster-bare-metal/roles/sysbench/files/run.oltp.sh
@@ -18,6 +18,7 @@ startmysql(){
 shutdownmysql(){
   echo "Shutting mysqld down..."
   systemctl stop mysql-cd
+  systemctl set-environment MYSQLD_OPTS=""
 }
 
 waitmysql(){
@@ -25,7 +26,7 @@ waitmysql(){
 
         while true;
         do
-                ${MYSQLDIR}mysql -Bse "SELECT 1" mysql
+                ${MYSQLDIR}mysql -h127.0.0.1 -Bse "SELECT 1" mysql
 
                 if [ "$?" -eq 0 ]
                 then
@@ -57,9 +58,21 @@ collect_dstat_stats(){
 
 shutdownmysql
 
-RUNDIR=res-mysql-oltp-`date +%F-%H-%M`
+RUNDIR=res-oltp-`hostname`-`date +%F-%H-%M`
+
+
+#server: mariadb
+#buffer_pool: 25
+#randtype: uniform
+#io_capacity: 15000
+#storage: NVMe
+
 echo "XFS defrag"
 xfs_fsr /dev/nvme0n1
+
+BP=25
+threads=150
+randtype="uniform"
 
 for io in 15000
 do
@@ -72,26 +85,36 @@ fstrim /data
 
 iomax=$(( 3*$io/2 ))
 
-startmysql "--innodb-io-capacity=${io} --innodb_io_capacity_max=$iomax" &
+startmysql "--innodb-io-capacity=${io} --innodb_io_capacity_max=$iomax --innodb_buffer_pool_size=${BP}GB" &
 sleep 10
 waitmysql
 
-runid="io$io"
+
 
 # perform warmup
 #./tpcc.lua --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=3600 --threads=56 --report-interval=1 --tables=10 --scale=100 --use_fk=1 run |  tee -a $OUTDIR/res.txt
 
-for i in 150
+for i in $threads
 do
+
+runid="io$io.BP${BP}.threads${i}"
 
         OUTDIR=$RUNDIR/$runid
         mkdir -p $OUTDIR
+
+echo "server: ps8" 		>> $OUTDIR/params.txt
+echo "buffer_pool: $BP" 	>> $OUTDIR/params.txt
+echo "randtype: $randtype" 	>> $OUTDIR/params.txt
+echo "io_capacity: $io" 	>> $OUTDIR/params.txt
+echo "threads: $i" 		>> $OUTDIR/params.txt
+echo "storage: NVMe" 		>> $OUTDIR/params.txt
+echo "host: `hostname`" 	>> $OUTDIR/params.txt
 
         # start stats collection
 
 
         time=10000
-        sysbench oltp_read_write --threads=$i --time=$time --tables=40 --table_size=10000000 --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --max-requests=0 --report-interval=1 --rand-type=uniform --mysql-db=sbtest --mysql-ssl=off --create_table_options='DEFAULT CHARSET=utf8mb4' --report_csv=yes run |  tee -a $OUTDIR/res.thr${i}.txt
+        sysbench oltp_read_write --threads=$i --time=$time --tables=40 --table_size=10000000 --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --max-requests=0 --report-interval=1 --mysql-db=sbtest --mysql-ssl=off --create_table_options='DEFAULT CHARSET=utf8mb4' --report_csv=yes --rand-type=$randtype run |  tee -a $OUTDIR/results.txt
 #        /mnt/data/vadim/bench/sysbench-tpcc/tpcc.lua --mysql-host=127.0.0.1 --mysql-user=sbtest --mysql-password=sbtest --mysql-db=sbtest --time=$time --threads=$i --report-interval=1 --tables=10 --scale=100 --use_fk=0 --report-csv=yes run |  tee -a $OUTDIR/res.thr${i}.txt
 
 
